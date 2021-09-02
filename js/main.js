f5e2536a906d89f1e47571fcb09329ad1c0eb3f9
Luca <lucaciprani1@gmail.com>
2021-09-02 18:28:29 +0200

    FontIcons Exerc

--- js/main.js
+++ js/main.js
@@ -1,160 +1,135 @@
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-
+// Datas
 const icons = [
     {
         name: 'cat',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'blue',
     },
     {
         name: 'crow',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'red',
     },
     {
         name: 'dog',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'yellow',
     },
     {
         name: 'dove',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'black',
     },
     {
         name: 'dragon',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'lightgray',
     },
     {
         name: 'horse',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'marron',
     },
     {
         name: 'hippo',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'lightgreen',
     },
     {
         name: 'fish',
         prefix: 'fa-',
         type: 'animal',
         family: 'fas',
-        // color: 'salmon',
     },
     {
         name: 'carrot',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'fas',
-        // color: 'green',
     },
     {
         name: 'apple-alt',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'fas',
-        // color: 'blue',
     },
     {
         name: 'lemon',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'fas',
-        // color: 'green',
     },
     {
         name: 'pepper-hot',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'fas',
-        // color: 'red',
     },
     {
         name: 'user-astronaut',
         prefix: 'fa-',
         type: 'user',
         family: 'fas',
-        // color: 'red',
     },
     {
         name: 'user-graduate',
         prefix: 'fa-',
         type: 'user',
         family: 'fas',
-        // color: 'black',
     },
     {
         name: 'user-ninja',
         prefix: 'fa-',
         type: 'user',
         family: 'fas',
-        // color: 'red',
     },
     {
         name: 'user-secret',
         prefix: 'fa-',
         type: 'user',
         family: 'fas',
-        // color: 'red',
     },
 ];
 
-// Milestone 2
-// Coloriamo le icone per tipo;
-
-// Milestone 3
-// Creiamo una select con i tipi di icone e usiamola per filtrare le icone;
-
+// added Datas;
 const colors = [
     'blue',
-    'orange',
-    'purple'
+    'green',
+    'red'
 ];
 
 
-const iconsContainer = document.getElementById('icons');
+// Milestone 1
+// Starting from data structure, show on the page all the icons available as per layout;
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+const print = (array, container) => {
+    container.innerHTML = '';  // to empty HTML content;
 
-
-// FOR THE CASE, WITHOUT A FUNCTION 
-
-// icons.forEach((element) => {
-//     iconsContainer.innerHTML = '';
-//     const {name, family, prefix, type} = element;
-//     iconsContainer.innerHTML +=
-//      `
-//     <div id="animal" class="square">
-//         <i class="${family} ${prefix}${name}" style= 'color:${color}'></i>
-//         <div class="title">${name.toUpperCase()}</div>
-//     </div>
-//     `
-// });
-
-
-
-// with a FUNCTION
+    array.forEach((element) => {
+        const { name, prefix, type, family, color } = element;
+        container.innerHTML += `
+        <div id="card" class="square">
+            <i class="${family} ${prefix}${name}" style= 'color:${color}'></i>
+            <div class="title">${name.toUpperCase()}</div>
+        </div>
+        `;
+    });
+  };
+  
+  // // with a normal FUNCTION
 
 // function print(array, container){
-//     iconsContainer.innerHTML = '';
+//     container.innerHTML = '';
 
 //     array.forEach((element) => {
-//         const {name, family, prefix, type} = element;
+//         const {name, family, prefix, type,} = element;
 //         container.innerHTML +=
 //          `
 //         <div id="animal" class="square">
@@ -166,57 +141,76 @@ const iconsContainer = document.getElementById('icons');
 //     });
 // }
 
-// print(icons, iconsContainer);
-
-
-
-// with an ARROW FUNCTION
-
-const print = (array, container) => {
-    iconsContainer.innerHTML = ''; // ripulisco il contenuto HTML;
-
-    array.forEach((element) => {
-        const {name, family, color, prefix, type} = element;
-        container.innerHTML +=
-        `
-        <div id="animal" class="square">
-            <i class="${family} ${prefix}${name}" style= 'color:${color}'></i>
-            <div class="title">${name.toUpperCase()}</div>
-            <div class="title">${type}</div>
-        </div>
-        `
-    });
-}
-
-print(icons, iconsContainer);
-console.log(getTypes(icons));
-
-function getTypes(array) {
+  // Milestone 2
+  // color the icons by type;
+  // Coloriamo le icone per tipo
+  const getTypes = (array) => {
     const types = [];
     array.forEach((element) => {
-        console.log(element);
-        if(types.includes(element.type) == false) {
-            types.push(element.type);
-        }
-
-    })
+      if(!types.includes(element.type)){
+        types.push(element.type);
+      }
+    });
     return types;
-}
-
-// inserisco i colori nell'array 
-
-function colorIcons(array, colors) {
+  };
+  
+  const colorIcons = (array, colors) => {
     const types = getTypes(array);
-    // console.log(types);
-}
+    const newArrayColored = array.map((element) =>{
+      const indexType = types.indexOf(element.type);
+      element.color = colors[indexType];
+      return element;
+    });
+    return newArrayColored;
+  };
+  
+  // Milestone 3
+  // Create a select with icons's types and use it to filter it;
+  // Creiamo una select con i tipi di icone e usiamola per filtrare le icone;
+  const printOptions = (array, select) => {
+    array.forEach((element) => {
+      select.innerHTML += 
+      `<option value="${element}">${element}</option>`;
+    });
+  };
+  
+  const filterValues = (array, type) =>{
+    const filteredIcons = array.filter((element) =>{
+      if(element.type === type) {
+        return true;
+      }
+      return false;
+    });
+    if(type === '') {
+      return array;
+    }
+    return filteredIcons;
+  };
+  
+  
+//   logic 
+  
+  const iconsContainer = document.getElementById('icons');
 
-console.log(colorIcons(icons, colors));
+  // new Array with colors after 'getTypes' and 'colorIcons' functions;
+  const newArrayColored = colorIcons(icons, colors);
+  print(newArrayColored, iconsContainer);
+  
+  // printing a new array colored with all 16 elements;
+  console.log(newArrayColored);  
+
+  const types = getTypes(newArrayColored);
+  console.log(types);
 
 
+  // variable 'select' in order to print in HTML the other options;
+  const select = document.getElementById('type');
+  printOptions(types, select);
+  console.log(select);
+  
 
-
-
-// --------------------
-// PER SVUOTARE SE CI FOSSE GIA QUALCOSA NELL'HTML inserisco:
-// iconsContainer.innerHTML = '';
-// --------------------
+//   onchange FUNCTION to filter objects based on properties;
+  select.onchange = function(element) {
+    const filtered = filterValues(icons, element.target.value);
+    print(filtered, iconsContainer);
+  };
