eacb8167a398efed106e3850ccee4d4c7eb28260
Luca <lucaciprani1@gmail.com>
2021-09-24 07:27:59 +0200

    update

--- js/main.js
+++ js/main.js
@@ -105,7 +105,6 @@ const colors = [
     'red'
 ];
 
-
 // Milestone 1
 // Starting from data structure, show on the page all the icons available as per layout;
 // Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
@@ -121,96 +120,81 @@ const print = (array, container) => {
         </div>
         `;
     });
-  };
-  
-  // // with a normal FUNCTION
+};
 
-// function print(array, container){
-//     container.innerHTML = '';
-
-//     array.forEach((element) => {
-//         const {name, family, prefix, type,} = element;
-//         container.innerHTML +=
-//          `
-//         <div id="animal" class="square">
-//             <i class="${family} ${prefix}${name}" style= 'color:${color}'></i>
-//             <div class="title">${name.toUpperCase()}</div>
-//             <div class="title">${type}</div>
-//         </div>
-//         `
-//     });
-// }
-
-  // Milestone 2
-  // color the icons by type;
-  // Coloriamo le icone per tipo
-  const getTypes = (array) => {
+// Milestone 2
+// color the icons by type;
+// Coloriamo le icone per tipo
+const getTypes = (array) => {
     const types = [];
     array.forEach((element) => {
-      if(!types.includes(element.type)){
-        types.push(element.type);
-      }
+        if (!types.includes(element.type)) {
+            types.push(element.type);
+        }
     });
+    console.log(types);
     return types;
-  };
-  
-  const colorIcons = (array, colors) => {
+};
+
+const colorIcons = (array, colors) => {
     const types = getTypes(array);
-    const newArrayColored = array.map((element) =>{
-      const indexType = types.indexOf(element.type);
-      element.color = colors[indexType];
-      return element;
+    console.log(types);
+    const newArrayColored = array.map((element) => {
+        const indexType = types.indexOf(element.type);
+        element.color = colors[indexType];
+        return element;
     });
     return newArrayColored;
-  };
-  
-  // Milestone 3
-  // Create a select with icons's types and use it to filter it;
-  // Creiamo una select con i tipi di icone e usiamola per filtrare le icone;
-  const printOptions = (array, select) => {
+};
+
+// Milestone 3
+// Create a select with icons's types and use it to filter it;
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone;
+const printOptions = (array, select) => {
     array.forEach((element) => {
-      select.innerHTML += 
-      `<option value="${element}">${element}</option>`;
+        select.innerHTML +=
+            `<option value="${element}">${element}</option>`;
     });
-  };
-  
-  const filterValues = (array, type) =>{
-    const filteredIcons = array.filter((element) =>{
-      if(element.type === type) {
-        return true;
-      }
-      return false;
+};
+
+const filterValues = (array, type) => {
+    const filteredIcons = array.filter((element) => {
+        if (element.type === type) {
+            return true;
+        }
+        return false;
     });
-    if(type === '') {
-      return array;
+    if (type === '') {
+        return array;
     }
     return filteredIcons;
-  };
-  
-  
-//   logic 
-  
-  const iconsContainer = document.getElementById('icons');
-
-  // new Array with colors after 'getTypes' and 'colorIcons' functions;
-  const newArrayColored = colorIcons(icons, colors);
-  print(newArrayColored, iconsContainer);
-  
-  // printing a new array colored with all 16 elements;
-  console.log(newArrayColored);  
-
-  const types = getTypes(newArrayColored);
-  console.log(types);
+};
 
 
-  // variable 'select' in order to print in HTML the other options;
-  const select = document.getElementById('type');
-  printOptions(types, select);
-  console.log(select);
-  
+//   LOGIC PART 
 
-//   onchange FUNCTION to filter objects based on properties;
-  select.onchange = function(element) {
+const iconsContainer = document.getElementById('icons');
+console.log(iconsContainer);
+
+// new Array with colors after 'getTypes' and 'colorIcons' functions;
+const newArrayColored = colorIcons(icons, colors);
+print(newArrayColored, iconsContainer);
+
+// printing a new array colored with all 16 elements;
+console.log(newArrayColored);
+
+const types = getTypes(newArrayColored);
+console.log(types);
+
+
+// variable 'select' in order to print in HTML the other options;
+const select = document.getElementById('type');
+printOptions(types, select);
+console.log(select);
+
+
+// 'onchange' FUNCTION to filter objects based on properties;
+select.onchange = function (element) {
     const filtered = filterValues(icons, element.target.value);
     print(filtered, iconsContainer);
-  };
+};
